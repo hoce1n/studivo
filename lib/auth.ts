b3dbcfc694d89ea59211dda83102c9c08ff6d5a9
hocein <1hccein@gmail.com>
2026-05-29 05:03:11 +0330

    first commit

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL!,
+});
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "sqlite",
+    }),
+});
