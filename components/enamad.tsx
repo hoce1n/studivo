0b1652269c4a33d095deac3ab37fd4e54429a5c5
hocein <1hccein@gmail.com>
2026-06-24 08:50:13 +0330

    enamad logo

---
+++ components/enamad.tsx
@@ -0,0 +1,18 @@
+export default function Enamad() {
+    return (
+        <>
+            <a 
+              referrerPolicy='origin' 
+              target='_blank' 
+              href='https://trustseal.enamad.ir/?id=6669149&Code=Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf'
+            >
+              <img 
+                  referrerPolicy='origin' 
+                  src='https://trustseal.enamad.ir/logo.aspx?id=6669149&Code=Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf' 
+                  alt='' 
+                  code='Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf' 
+                />
+            </a>
+        </>
+    )
+}
