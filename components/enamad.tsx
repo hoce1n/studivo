6f24462270989d19026a2a947b2d0a3cb16f2fc3
hocein <1hccein@gmail.com>
2026-06-24 09:45:13 +0330

    fixing enamad components

--- components/enamad.tsx
+++ components/enamad.tsx
@@ -1,18 +1,18 @@
 export default function Enamad() {
-    return (
-        <>
-            <a 
-              referrerPolicy='origin' 
-              target='_blank' 
-              href='https://trustseal.enamad.ir/?id=6669149&Code=Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf'
-            >
-              <img 
-                  referrerPolicy='origin' 
-                  src='https://trustseal.enamad.ir/logo.aspx?id=6669149&Code=Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf' 
-                  alt='' 
-                  code='Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf' 
-                />
-            </a>
-        </>
-    )
+  return (
+    <a
+      referrerPolicy="origin"
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://trustseal.enamad.ir/?id=6669149&Code=Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf"
+    >
+      <img
+        referrerPolicy="origin"
+        src="https://trustseal.enamad.ir/logo.aspx?id=6669149&Code=Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf"
+        alt="نماد اعتماد الکترونیکی"
+      />
+    </a>
+  );
 }
+
+// code='Ney2CGOcVX7FPSJ7VP3ZxVedBqaPRBOf' 
